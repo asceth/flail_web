a24f9439977ccce3694155d111659d17f37f45d9
John Long <john.long@ors.sc.gov>
2012-07-17 20:32:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FlailWeb::Application.config.secret_token = '47aee8fbc6119a1d5b5691e4b1944a1fd14702dc760280220cdb03efc1382d8ae6792912758474825af61f2863a464b3437d5520835b71ff768b7b039858a1e9'
